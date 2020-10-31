174e510f267cce96e8987ed68a16f490fdb7c9f1
Dominik Nakamura <dnaka91@gmail.com>
2020-11-01 00:58:14 +0900

    Add menu and help area

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@
 #![warn(clippy::nursery)]
 
 use std::io;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use std::sync::mpsc::{self, Receiver};
 use std::thread;
 use std::time::Duration;
 
@@ -13,10 +12,14 @@ use clap::{AppSettings, Clap};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode, screen::AlternateScreen};
 use tui::{
     backend::{Backend, TermionBackend},
+    layout::{Margin, Rect},
+    style::Color,
+    style::Style,
+    widgets::{Clear, Paragraph},
     Terminal,
 };
 
-use crate::matrix::{Rain, RainState};
+use crate::matrix::{KanaBorder, KanaBorderState, KanaList, KanaListState, Rain, RainState};
 
 mod matrix;
 #[cfg(feature = "twitch")]
@@ -35,6 +38,23 @@ struct Opt {
     twitch_username: String,
 }
 
+#[derive(Eq, PartialEq)]
+enum Showing {
+    Nothing,
+    Menu,
+    Help,
+}
+
+const HELP_TEXT: &str = "\
+Welcome to rsmatrix a Matrix rain screensaver written in Rust.
+
+The following commands can be used:
+
+  - h toggle this help message
+  - m toggle the menu to navigate to different areas
+  - q quit the application\
+";
+
 fn main() -> Result<()> {
     let opt = Opt::parse();
 
@@ -44,21 +64,91 @@ fn main() -> Result<()> {
     let namelist = vec!["test".to_owned()];
 
     let mut terminal = create_terminal()?;
-    let stop = create_shutdown();
+    let events = create_event_listener();
 
     let sleep_time = Duration::from_millis(1000 / opt.fps);
     let mut state = RainState::new(Duration::from_millis(1000 / opt.dps));
+    let mut border_state = KanaBorderState::default();
+    let mut list_state = KanaListState::default();
+    let mut showing = Showing::Nothing;
 
-    loop {
+    let list_items = &["Item 1", "Item 2", "Item 3", "Item 4"];
+
+    'drawloop: loop {
         terminal.draw(|f| {
             let size = f.size();
             f.render_stateful_widget(Rain::new(47, &namelist), size, &mut state);
+
+            match showing {
+                Showing::Menu => {
+                    let list = KanaList::new(list_items);
+
+                    let r = Rect::new(0, 0, 40, 4 + list_items.len() as u16);
+                    let r = r.center_in(size);
+
+                    f.render_widget(Clear, r);
+                    f.render_stateful_widget(KanaBorder, r, &mut border_state);
+
+                    let r = r.inner(&Margin {
+                        vertical: 2,
+                        horizontal: 3,
+                    });
+                    f.render_stateful_widget(list, r, &mut list_state);
+                }
+                Showing::Help => {
+                    let r = Rect::new(0, 0, 68, 11).center_in(size);
+
+                    f.render_widget(Clear, r);
+                    f.render_stateful_widget(KanaBorder, r, &mut border_state);
+
+                    let r = r.inner(&Margin {
+                        vertical: 2,
+                        horizontal: 3,
+                    });
+
+                    f.render_widget(
+                        Paragraph::new(HELP_TEXT).style(Style::default().fg(Color::Indexed(47))),
+                        r,
+                    );
+                }
+                Showing::Nothing => {}
+            }
         })?;
 
         thread::sleep(sleep_time);
 
-        if stop.load(Ordering::Relaxed) {
-            break;
+        while let Ok(event) = events.try_recv() {
+            match event {
+                KeyEvent::Quit => break 'drawloop,
+                KeyEvent::ToggleMenu => {
+                    showing = match showing {
+                        Showing::Menu => Showing::Nothing,
+                        _ => Showing::Menu,
+                    }
+                }
+                KeyEvent::ToggleHelp => {
+                    showing = match showing {
+                        Showing::Help => Showing::Nothing,
+                        _ => Showing::Help,
+                    }
+                }
+                KeyEvent::MoveUp => {
+                    if showing == Showing::Menu {
+                        list_state.prev(list_items);
+                    }
+                }
+                KeyEvent::MoveDown => {
+                    if showing == Showing::Menu {
+                        list_state.next(list_items);
+                    }
+                }
+                KeyEvent::Select => {
+                    if matches!(showing, Showing::Menu) {
+                        showing = Showing::Nothing;
+                    }
+                    // TODO: Do something with the menu item
+                }
+            }
         }
     }
 
@@ -72,22 +162,52 @@ fn create_terminal() -> Result<Terminal<impl Backend>> {
     Terminal::new(backend).map_err(Into::into)
 }
 
-fn create_shutdown() -> Arc<AtomicBool> {
-    let stop = Arc::new(AtomicBool::new(false));
-    let s = stop.clone();
+enum KeyEvent {
+    Quit,
+    ToggleMenu,
+    ToggleHelp,
+    MoveUp,
+    MoveDown,
+    Select,
+}
+
+fn create_event_listener() -> Receiver<KeyEvent> {
+    let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
         let mut keys = io::stdin().keys();
 
         while let Some(Ok(k)) = keys.next() {
-            match k {
-                Key::Esc | Key::Char('q') | Key::Ctrl('c') => break,
-                _ => {}
+            let event = match k {
+                Key::Esc | Key::Char('q') | Key::Ctrl('c') => Some(KeyEvent::Quit),
+                Key::Char('m') => Some(KeyEvent::ToggleMenu),
+                Key::Char('h') => Some(KeyEvent::ToggleHelp),
+                Key::Up => Some(KeyEvent::MoveUp),
+                Key::Down => Some(KeyEvent::MoveDown),
+                Key::Char('\n') => Some(KeyEvent::Select),
+                _ => None,
+            };
+
+            if let Some(event) = event {
+                tx.send(event).ok();
             }
         }
-
-        s.store(true, Ordering::Relaxed);
     });
 
-    stop
+    rx
+}
+
+trait RectExt {
+    fn center_in(self, outer: Self) -> Self;
+}
+
+impl RectExt for Rect {
+    fn center_in(self, outer: Self) -> Self {
+        Self::new(
+            (outer.width - self.width) / 2,
+            (outer.height - self.height) / 2,
+            self.width,
+            self.height,
+        )
+    }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -12,13 +12,12 @@ use tui::{
 
 #[derive(Copy, Clone)]
 pub struct Rain<'a> {
-    color: u8,
     namelist: &'a [String],
 }
 
 impl<'a> Rain<'a> {
-    pub const fn new(color: u8, namelist: &'a [String]) -> Self {
-        Self { color, namelist }
+    pub const fn new(_color: u8, namelist: &'a [String]) -> Self {
+        Self { namelist }
     }
 }
 
@@ -54,6 +53,53 @@ impl<'a> Element<'a> {
             .collect();
         self.pos = (rng.gen::<u16>() % area.right(), 0);
     }
+
+    fn update_active(&mut self, area: Rect) -> bool {
+        if self.pos.1 as usize >= area.bottom() as usize + self.name.len() + self.trail.len()
+            || self.pos.0 >= area.right()
+        {
+            self.active = false;
+        }
+
+        self.active
+    }
+
+    fn draw_name(&self, area: Rect, buf: &mut Buffer) {
+        for (i, c) in self.name.chars().rev().enumerate() {
+            if let Some(pos) = self.pos.1.checked_sub(i as u16) {
+                if pos < area.bottom() {
+                    buf.get_mut(self.pos.0, pos)
+                        .set_style(
+                            Style::default()
+                                .fg(Color::Indexed(47))
+                                .bg(Color::Indexed(23))
+                                .add_modifier(Modifier::BOLD),
+                        )
+                        .set_char(c);
+                }
+            }
+        }
+    }
+
+    fn draw_tail(&self, area: Rect, buf: &mut Buffer) {
+        for (i, c) in self.trail.iter().enumerate() {
+            if let Some(pos) = self.pos.1.checked_sub((self.name.len() + i) as u16) {
+                if pos < area.bottom() {
+                    buf.get_mut(self.pos.0, pos)
+                        .set_fg(Color::Indexed(if i < self.trail.len() / 2 {
+                            35
+                        } else {
+                            23
+                        }))
+                        .set_char(*c);
+                }
+            }
+        }
+    }
+
+    fn step(&mut self) {
+        self.pos.1 += 1;
+    }
 }
 
 fn random_char(rng: &mut impl Rng) -> char {
@@ -64,6 +110,10 @@ fn random_char(rng: &mut impl Rng) -> char {
     }
     .choose(rng)
     .unwrap()
+}
+
+fn random_katakana(rng: &mut impl Rng) -> char {
+    ('\u{ff66}'..='\u{ff9d}').choose(rng).unwrap()
 }
 
 impl<'a> StatefulWidget for Rain<'a> {
@@ -90,48 +140,117 @@ impl<'a> StatefulWidget for Rain<'a> {
 
         // Draw all active raindrops.
         for element in state.elements.iter_mut().filter(|e| e.active) {
-            // If out of sight, deactivate the element.
-            if element.pos.1 as usize
-                >= area.bottom() as usize + element.name.len() + element.trail.len()
-                || element.pos.0 >= area.right()
-            {
-                element.active = false;
+            if !element.update_active(area) {
                 continue;
             }
 
-            // Draw the name first.
-            for (i, c) in element.name.chars().rev().enumerate() {
-                if let Some(pos) = element.pos.1.checked_sub(i as u16) {
-                    if pos < area.bottom() {
-                        buf.get_mut(element.pos.0, pos)
-                            .set_style(
-                                Style::default()
-                                    .fg(Color::Indexed(self.color))
-                                    .bg(Color::Indexed(self.color - 24))
-                                    .add_modifier(Modifier::BOLD),
-                            )
-                            .set_char(c);
-                    }
-                }
+            element.draw_name(area, buf);
+            element.draw_tail(area, buf);
+
+            element.step();
+        }
+    }
+}
+
+pub struct KanaBorder;
+
+#[derive(Default)]
+pub struct KanaBorderState {
+    chars: Vec<char>,
+}
+
+impl StatefulWidget for KanaBorder {
+    type State = KanaBorderState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        let rng = &mut rand::thread_rng();
+
+        (area.left()..area.right())
+            .map(|x| (x, area.top()))
+            .chain((area.left()..area.right()).map(|x| (x, area.bottom() - 1)))
+            .chain((area.top() + 1..area.bottom() - 1).map(|y| (area.left(), y)))
+            .chain((area.top() + 1..area.bottom() - 1).map(|y| (area.right() - 1, y)))
+            .enumerate()
+            .for_each(|(i, (x, y))| {
+                let c = if let Some(c) = state.chars.get(i) {
+                    *c
+                } else {
+                    let c = random_katakana(rng);
+                    state.chars.push(c);
+                    c
+                };
+
+                buf.get_mut(x, y).set_char(c).set_style(
+                    Style::default()
+                        .fg(Color::Indexed(35))
+                        .add_modifier(Modifier::BOLD),
+                );
+            });
+    }
+}
+
+pub struct KanaList<'a> {
+    items: &'a [&'a str],
+}
+
+impl<'a> KanaList<'a> {
+    pub const fn new(items: &'a [&'a str]) -> Self {
+        Self { items }
+    }
+}
+
+pub struct KanaListState {
+    selected: usize,
+    pointer: char,
+    last_update: Instant,
+}
+
+impl Default for KanaListState {
+    fn default() -> Self {
+        Self {
+            selected: 0,
+            pointer: random_katakana(&mut rand::thread_rng()),
+            last_update: Instant::now(),
+        }
+    }
+}
+
+impl KanaListState {
+    pub fn next(&mut self, items: &[&str]) {
+        self.selected = (self.selected + 1) % items.len();
+    }
+
+    pub fn prev(&mut self, items: &[&str]) {
+        if self.selected == 0 {
+            self.selected = items.len() - 1;
+        } else {
+            self.selected -= 1;
+        }
+    }
+}
+
+const POINTER_REFRESH_TIME: Duration = Duration::from_millis(400);
+
+impl<'a> StatefulWidget for KanaList<'a> {
+    type State = KanaListState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        for (i, item) in self.items.iter().enumerate() {
+            let mut style = Style::default().fg(Color::Indexed(47));
+
+            if state.last_update.elapsed() > POINTER_REFRESH_TIME {
+                state.pointer = random_katakana(&mut rand::thread_rng());
+                state.last_update = Instant::now();
             }
 
-            // Then the trail.
-            for (i, c) in element.trail.iter().enumerate() {
-                if let Some(pos) = element.pos.1.checked_sub((element.name.len() + i) as u16) {
-                    if pos < area.bottom() {
-                        buf.get_mut(element.pos.0, pos)
-                            .set_fg(Color::Indexed(if i < element.trail.len() / 2 {
-                                self.color - 12
-                            } else {
-                                self.color - 24
-                            }))
-                            .set_char(*c);
-                    }
-                }
+            if i == state.selected {
+                style = style.add_modifier(Modifier::BOLD);
+                buf.get_mut(area.left(), area.top() + i as u16)
+                    .set_style(style)
+                    .set_char(state.pointer);
             }
 
-            // Move the element forward.
-            element.pos.1 += 1;
+            buf.set_string(area.left() + 2, area.top() + i as u16, item, style);
         }
     }
 }
